9dc7a94b31a9b84bc6f36b998da7786bba37b7bd
Sebastian Martinez <smartinez@nuclearis.com>
2021-07-16 10:34:34 -0300

    feat: add final works text

---
+++ src/pages/worksContent.js
@@ -0,0 +1,125 @@
+const worksContent = [
+  {
+    id: "work_1",
+    title: "Electroplating Machine 1.0",
+    shortText: "For Siemens KWU Nuclear Power Plant",
+    overlayText:
+      `<p>NUCLEARIS is a qualified supplier of Seal Rings, class 1 components, for PHWR 
+      (pressurized heavy water reactor) Nuclear Power Plants. These components are used in 
+      SIEMENS-KWU nuclear power plants, and they are metallic seals covered in electrolytic 
+      silver functioning as sealing of the cooling channels where uranium fuel elements are.`,
+    fullText: `<p class="mb-2">NUCLEARIS is a qualified supplier of Seal Rings, class 1 components, 
+      for PHWR (pressurized heavy water reactor) Nuclear Power Plants. These components 
+      are used in SIEMENS-KWU nuclear power plants, and they are metallic seals covered 
+      in electrolytic silver functioning as sealing of the cooling channels where uranium 
+      fuel elements are. These rings seal the pressure vessel in hundreds of cooling channels inside
+      the primary circuit of the reactor, where the heavy water circulates at 570°F of 
+      temperature and 120 Atm of pressure.</p><p class="mb-2">NUCLEARIS designed and built a machine 
+      that automates the electroplating process of these critical parts.</p><p class="mb-2">This is the most 
+      important process in Seal Rings manufacturing because sealing capability depends upon the 
+      resistance and adhesion of the Silver covering. These rings must resist inside the reactor 
+      for a whole year and are subject to a heavy neutronic radiation environment. The covering is 
+      a coat of 99.99% of pure Silver of 80 microns.</p><p class="mb-2">NUCLEARIS made a high technological 
+      solution for a process that was made manually in Germany for many decades.</p>`,
+  },
+  {
+    id: "work_2",
+    title: "Decay Pool Safety System",
+    shortText: "For Siemens KWU Nuclear Power Plant",
+    overlayText:
+      `<p class="mb-2">NUCLEARIS developed the engineering, construction, and startup of an automated operational safety system to reduce maneuvering of the bridge handling burned uranium fuels.</p>
+      <p class="mb-2">The decay pools have demineralized water that cools down the spent fuels and acts as a biological shield for radiation.</p>`,
+    fullText:
+      `<p class="mb-2">NUCLEARIS developed the engineering, construction, and startup of an automated 
+      operational safety system to reduce maneuvering of the bridge handling burned uranium fuels.</p> 
+      <p class="mb-2">The decay pools have demineralized water that cools down the spent fuels and acts as a biological shield for radiation.</p>
+      <p class="mb-2">This development is an anti-collision System to prevent different objects handled underwater collide 
+      with other existing objects inside the pool. NUCLEARIS adapted the old mechanic system and outfitted it 
+      with the latest digital sensors and digital encoders in an automated system that restricts the bridge 
+      movement in its three axes, allowing it to only move within a determined space called “virtual volume” 
+      which is smaller than the pool’s volume. In case an object reaches the geometric limit of such “virtual volume” 
+      the system will automatically prevent the bridge maneuvers.</p>
+      <p>NUCLEARIS has developed an intelligent system with pre-loaded information related to the geometry of 
+      thousands of objects inside the decay pools. Moreover, it can detect which type of objects or elements 
+      are pending from the bridge. This system eliminates the risk of colliding a spent fuel element against 
+      other submerged elements, the pool walls, or the floors.</p>`
+  },
+  {
+    id: "work_3",
+    title: "Nuclear Mechanical Encoders",
+    shortText: "For Siemens KWU Nuclear Power Plant",
+    overlayText:
+      `NUCLEARIS, through its qualified machining partners, has developed the production of high precision 
+      metal discs for the positioning system of the refueling machine of SIEMENS-KWU nuclear power plants. 
+      This machine is in charge of extracting spent uranium fuel elements and introducing new ones inside 
+      the vessel of the NPP.`,
+    fullText:
+      `<p class="mb-2">NUCLEARIS, through its qualified machining partners, has developed the production of high precision 
+      metal discs for the positioning system of the refueling machine of SIEMENS-KWU nuclear power plants. 
+      This machine is in charge of extracting spent uranium fuel elements and introducing new ones inside 
+      the vessel of the NPP.</p>
+      <p class="mb-2">These rotating discs are part of the so-called encoders, which convert an electrical signal into linear 
+      position information. Modern Optical Encoders cannot be used inside a nuclear reactor because the electronic 
+      micro components do not resist neutron radiation, thus, electromechanical encoders must be used instead. 
+      These encoders use several metallic brushes that function as “electrical contacts” that slide on the “stepped” 
+      disc. This way the brushes conduct electrical signals through the metal areas, while they do not conduct 
+      electricity through the areas where the plastic resin is located.</p>
+      <p class="mb-2">The first part of the development consisted of machining the martensitic stainless steel discs by EDM and 
+      then adding a plastic resin to generate the areas without electrical conductivity.</p>
+      <p>After the machining process, NUCLEARIS developed the Gold Electroplating (24klts) process over the encoder 
+      disc. The Gold covering over the disc must be of high precision to assure correct positioning of the refueling 
+      machine inside the reactor building.</p>`
+  },
+  {
+    id: "work_4",
+    title: "Electroplating Machine 2.0",
+    shortText: "For CANDU Type Nuclear Power Plant",
+    overlayText:
+      `NUCLEARIS has designed and manufactured two mechanical class 1 components (Component for the nuclear 
+      island in an NPP) for CANDU (Canadian Deuterium Uranium) reactors from Canada. These mechanical 
+      components have a key role in the reactor operation and their function is to seal the heavy water 
+      in the primary circuit and inside the end fittings.`,
+    fullText:
+      `<p class="mb-2">NUCLEARIS has designed and manufactured two mechanical class 1 components (Component for the nuclear 
+      island in an NPP) for CANDU (Canadian Deuterium Uranium) reactors from Canada. These mechanical 
+      components have a key role in the reactor operation and their function is to seal the heavy water 
+      in the primary circuit and inside the end fittings.</p>
+      <p class="mb-2">One of them is called Seal Insert and consists of a steel ring covered with an electrolytic layer of 24 
+      Karat of Gold, which is assembled with another mechanical part called Seal Disc, which has the function 
+      of sealing the channel where the Uranium fuels. This latter piece also has a high resistance nickel 
+      electrolytic coating.</p>
+      <p class="mb-2">NUCLEARIS has specially developed a machine for gold and nickel covering for CANDU components.</p>
+      <p class="mb-2">For this development, NUCLEARIS implemented the use of a FANUC robot with 6 degrees of freedom, which 
+      provides maximum versatility, reliability, and speed in the production process. For the electrolytic 
+      tanks, we have created independent units made of polypropylene and equipped them with a high-efficiency 
+      recirculation and filtering system.</p>
+      `
+  },
+  {
+    id: "work_5",
+    title: "Irradiation Head",
+    shortText: "For Research Nuclear Reactor RA10",
+    overlayText:
+      `<p class="mb-1">NUCLEARIS has developed detailed engineering of a testing device for a new multipurpose research nuclear reactor called RA10 for the National Commission of Atomic Energy (CNEA) of Argentina.</p>
+      <p>This device allows the evaluation of the thermal and neutronic performance of the fuel rods for different NPPs. NUCLEARIS developed this testing device under ASME CODE section III.</p>`,
+    fullText:
+      `<p class="mb-2">NUCLEARIS has developed detailed engineering of a testing device for a new multipurpose research nuclear reactor called RA10 for the National Commission of Atomic Energy (CNEA) of Argentina.</p>
+      <p class="mb-2">This device allows the evaluation of the thermal and neutronic performance of the fuel rods for different NPPs. NUCLEARIS developed this testing device under ASME CODE section III.</p>
+      <p class="mb-2">The RA-10 reactor was designed as a multipurpose facility suitable for RadioIsotope production, materials and fuel irradiation, neutron techniques applications, and silicon doping. Its design is based on SEU (Slightly enriched uranium) fuel elements and meets the Argentinian Safety Regulations and International Atomic Energy Agency (IAEA) Standards.</p>
+      <p class="mb-2">NUCLEARIS was in charge of the design, engineering, calculation, and verification by finite element method, manufacturing the mockup and all mechanical components, assembling, and qualified welding.</p>`
+  },
+  {
+    id: "work_6",
+    title: "Primary Circuit Valve",
+    shortText: "For Siemens KWU Nuclear Power Plant",
+    overlayText:
+      `The PHWR (pressurized heavy water reactors) Nuclear Power Plants from Siemens-KWU were designed in the 1970s, and today some spare parts are no longer manufactured in their country of origin. NUCLEARIS has developed various mechanical components that have been discontinued by the original manufacturers, in Germany.`,
+    fullText:
+      `<p class="mb-2">The PHWR (pressurized heavy water reactors) Nuclear Power Plants from Siemens-KWU were designed in the 1970s, and today some spare parts are no longer manufactured in their country of origin. NUCLEARIS has developed various mechanical components that have been discontinued by the original manufacturers, in Germany.</p>
+      <p class="mb-2">In this case, we have reverse-engineered a valve from the heavy water primary circuit and manufactured prototypes to test in a mock-up until we obtained a final qualification according to the technical requirements of the plant.</p>
+      <p class="mb-2">All parts were produced by our machining partners under ISO9001. Additionally, the various mechanical parts were assembled by TIG welding at NUCLEARIS using the procedures approved by the Nuclear Power Plant.</p>
+      <p class="mb-2">Today, NUCLEARIS is the only supplier qualified to manufacture this class 1 component (Component for the nuclear island in an NPP)</p>`,
+  },
+]
+
+export default worksContent
